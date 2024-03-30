ad562eedfbbb80c9825e92f2f90c98cc110c4a2e
RukoBlood <coomer.obama@gmail.com>
2024-03-30 15:57:27 +0700

    0.0.0.2 Еда теперь отдельным классом (Раньше было внутри итем класса) Насвай теперь даёт реген и скорость 2 на 5 сек Также с шансом в 10% выпадает тошнота 2 на 10 сек.

---
+++ src/main/java/com/gaq35ms2/naswaymod/items/NaswayModFood.java
@@ -0,0 +1,17 @@
+package com.gaq35ms2.naswaymod.items;
+
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.item.FoodComponent;
+
+public class NaswayModFood {
+    public static final FoodComponent Nasway = new FoodComponent.Builder()
+            .snack()
+            .alwaysEdible()
+            .hunger(4)
+            .saturationModifier(2.4f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.REGENERATION, 100), 1f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.SPEED, 100, 2), 1f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.NAUSEA, 200,2), 0.1f)
+            .build();
+}
--- src/main/java/com/gaq35ms2/naswaymod/items/NaswayModItems.java
+++ src/main/java/com/gaq35ms2/naswaymod/items/NaswayModItems.java
@@ -4,7 +4,6 @@ import com.gaq35ms2.naswaymod.NaswayMod;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
-import net.minecraft.item.FoodComponent;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
@@ -12,9 +11,9 @@ import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
 public class NaswayModItems {
-    public static final Item Nasway = LRegisteritem("nasway", new Item(new FabricItemSettings()
-            .food(new FoodComponent.Builder().snack().hunger(2).saturationModifier(0.4f).build())));
-
+    //Насвай перенесён в отдельный класс с едой.
+    //public static final Item Nasway = LRegisteritem("nasway", new Item(new FabricItemSettings().food(new FoodComponent.Builder().snack().hunger(2).saturationModifier(0.4f).build())));
+    public static final Item Nasway = LRegisteritem("nasway", new Item(new FabricItemSettings().food(NaswayModFood.Nasway)));
     private static void LAddItemToItemGroup(FabricItemGroupEntries entries){
         entries.add(Nasway);
     }
@@ -22,7 +21,7 @@ public class NaswayModItems {
         return Registry.register(Registries.ITEM, new Identifier(NaswayMod.MOD_ID, name), item);
     }
     public static void RegisterItems() {
-        NaswayMod.LOGGER.info("Registering" + NaswayMod.MOD_ID + "Items...");
+        NaswayMod.LOGGER.info("Registering " + NaswayMod.MOD_ID + " Items...");
 
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.FOOD_AND_DRINK).register(NaswayModItems::LAddItemToItemGroup);
     }
