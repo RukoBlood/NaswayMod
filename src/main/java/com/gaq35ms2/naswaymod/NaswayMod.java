da54df533a82ce246e33825ac238aecae3f25fad
RukoBlood <coomer.obama@gmail.com>
2024-03-30 13:03:09 +0700

    Mod teperb rabotaet

---
+++ src/main/java/com/gaq35ms2/naswaymod/NaswayMod.java
@@ -0,0 +1,13 @@
+package com.gaq35ms2.naswaymod;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+public class NaswayMod implements ModInitializer{
+    public static final String MOD_ID = "naswaymod";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+    @Override
+    public void onInitialize(){
+        LOGGER.info("NaswayMod very unstable release");
+    }
+}
